836016d952444c79dec3d426b99abd467d84489f
M4T <mateo.lesiuk@gmail.com>
2019-02-25 08:08:21 +0100

    Install sass,css,style loaders and @babel/preset-env

--- webpack.config.js
+++ webpack.config.js
@@ -7,10 +7,27 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader', // creates style nodes from JS strings
+          'css-loader', // translates CSS into CommonJS
+          'sass-loader' // compiles Sass to CSS, using Node Sass by default
+        ],
+      },
+    ],
+  },
   plugins: [
     new HTMLWebpackPlugin({
       template: './src/index.html',
-      filename: 'index.html',
+      filename: 'index.html'
     }),
   ],
 };
