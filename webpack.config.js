5d90128a72a1508aaff769744af842d94aa33114
M4T <mateo.lesiuk@gmail.com>
2019-02-25 07:36:54 +0100

    Add Webpack config file and install HTMLWebpackPlugin

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/js/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [new HTMLWebpackPlugin()],
+};
