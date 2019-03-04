43db36810a33f688416739a1352ce7fc046de750
M4T <mateo.lesiuk@gmail.com>
2019-03-04 08:21:35 +0100

    Temp config file

---
+++ src/js/config.js
@@ -0,0 +1,7 @@
+/**
+ * TODO: change APIKEY to get from environment on server
+ */
+export const APIKEY = 'fa260932a13e9408391c2ac7b75e9548';
+export const PROXY = 'https://cors-proxify.herokuapp.com/';
+export const CitiesAPI = 'http://cities-ids.herokuapp.com';
+export const units = 'metric';
