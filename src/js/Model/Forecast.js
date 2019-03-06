6f2e8087893566124015e78595eaa9ac755a257a
M4T <mateo.lesiuk@gmail.com>
2019-03-06 15:08:15 +0100

    Create Forecast model and view

---
+++ src/js/Model/Forecast.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import { APIKEY, PROXY } from '../config';
+
+export default class Forecast {
+  constructor() {
+    this.id = Number;
+  }
+
+  async getWeather(id) {
+    try {
+      const res = await axios(
+        `${PROXY}https://api.openweathermap.org/data/2.5/forecast?appid=${APIKEY}&units=metric&id=${id}`
+      );
+
+      this.weather = res.data.list;
+      this.city = res.data.city;
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+  }
+
+  clearWeather() {
+    this.weather = [];
+    this.city = {};
+  }
+}
---
+++ src/js/View/forecastView.js
@@ -0,0 +1,61 @@
+import { elements, elementsString } from './base';
+
+/**
+ * MAYBE TODO:
+ * display city name above weather or just 'Forecast'
+ */
+
+export const renderForecast = city => {
+  const markup = `
+		<div class="forecast-nav">
+			<img src="images/placeholder.svg" alt="Location icon" class="location-icon" />
+			<div class="forecast-city-name">
+				Forecast
+			</div>
+			<button class="close-button">
+				<img src="images/close.svg" alt="Close icon" class="close-forecast-btn-icon">
+			</button>
+		</div>
+
+		<div class="forecast-days">
+
+
+
+		</div>
+	`;
+
+  elements.mainContainer.insertAdjacentHTML('afterbegin', markup);
+};
+
+export const renderWeather = (weather, city) => {
+  const markup = `
+		<div class="day">
+			<span class="date">${weather.dt_txt}</span>
+			<div class="day-section">
+				<h2 class="day-section-city-name">${city.name}</h2>
+			</div>
+			<div class="day-section">
+				<div class="day-section-weather">
+					<img src="images/weather-icons/sun black.svg" alt="Weather icon" class="day-section-weather-icon">
+					<span class="day-section-weather-description">${
+            weather.weather.description
+          }</span>
+				</div>
+			</div>
+			<div class="day-section">
+				<div class="day-section-temperature">
+					<span class="day-section-main-temperature">${Math.round(
+            weather.main.temp
+          )}°C</span>
+					<span class="day-section-min-max-temperature">${Math.round(
+            weather.main.temp_min
+          )} • ${Math.round(weather.main.temp_max)}</span>
+				</div>
+			</div>
+		</div>
+	`;
+
+  document
+    .querySelector(`.${elementsString.forecastDays}`)
+    .insertAdjacentHTML('beforeend', markup);
+};
